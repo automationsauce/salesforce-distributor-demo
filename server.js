c81862f5adf0e171a31f86d040d34ae28df0aeb7
automationsauce <josh@automationsauce.com>
2026-05-16 11:06:22 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+const {
+  evaluateCriterion,
+  evaluateLogic
+} = require("./services/logicService");
+
 let accounts = {};
 
 app.use(express.json());
@@ -33,41 +38,6 @@ app.get("/oauth/callback", (req, res) => {
   `);
 });
 
-function evaluateCriterion(record, criterion) {
-  const recordValue = record[criterion.field];
-  const targetValue = criterion.value;
-
-  switch (criterion.operator) {
-    case "=":
-      return String(recordValue) === String(targetValue);
-    case "!=":
-      return String(recordValue) !== String(targetValue);
-    case ">":
-      return Number(recordValue) > Number(targetValue);
-    case "<":
-      return Number(recordValue) < Number(targetValue);
-    case ">=":
-      return Number(recordValue) >= Number(targetValue);
-    case "<=":
-      return Number(recordValue) <= Number(targetValue);
-    default:
-      return false;
-  }
-}
-
-function evaluateLogic(logic, results) {
-  let expression = logic || "";
-
-  for (const sequence in results) {
-    expression = expression.replaceAll(sequence, String(results[sequence]));
-  }
-
-  expression = expression
-    .replaceAll("AND", "&&")
-    .replaceAll("OR", "||");
-
-  return Function(`return (${expression});`)();
-}
 
 function getNextAgentForDistributor(account, distributor) {
   const activeAgents = (account.agents || [])
