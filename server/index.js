5ed5f472695ebe046d71502b794a0900000b82e1
automationsauce <josh@automationsauce.com>
2026-06-10 09:44:41 +0100

    AssignmentService Split
    Assignment Service Split logic.

--- server/index.js
+++ server/index.js
@@ -12,6 +12,10 @@ const {
   updateDistributorNextAgent
 } = require("./crm/salesforce/salesforceService");
 
+const {
+  getNextAgentForDistributor
+} = require("./services/assignmentService");
+
 let accounts = {};
 
 app.use(express.json());
@@ -46,46 +50,46 @@ app.get("/oauth/callback", (req, res) => {
 });
 
 
-function getNextAgentForDistributor(account, distributor) {
-  const activeAgents = (account.agents || [])
-    .filter(agent =>
-      agent.distributionId === distributor.id &&
-      agent.active === true
-    )
-    .sort((a, b) => Number(a.sequence) - Number(b.sequence));
+// function getNextAgentForDistributor(account, distributor) {
+//   const activeAgents = (account.agents || [])
+//     .filter(agent =>
+//       agent.distributionId === distributor.id &&
+//       agent.active === true
+//     )
+//     .sort((a, b) => Number(a.sequence) - Number(b.sequence));
 
-  if (activeAgents.length === 0) {
-    return {
-      agent: null,
-      nextAgentSequence: distributor.nextAgent || null
-    };
-  }
+//   if (activeAgents.length === 0) {
+//     return {
+//       agent: null,
+//       nextAgentSequence: distributor.nextAgent || null
+//     };
+//   }
 
-  const nextSequence = Number(distributor.nextAgent || activeAgents[0].sequence);
+//   const nextSequence = Number(distributor.nextAgent || activeAgents[0].sequence);
 
-  let selectedIndex = activeAgents.findIndex(
-    agent => Number(agent.sequence) === nextSequence
-  );
+//   let selectedIndex = activeAgents.findIndex(
+//     agent => Number(agent.sequence) === nextSequence
+//   );
 
-  if (selectedIndex === -1) {
-    selectedIndex = 0;
-  }
+//   if (selectedIndex === -1) {
+//     selectedIndex = 0;
+//   }
 
-  const selectedAgent = activeAgents[selectedIndex];
+//   const selectedAgent = activeAgents[selectedIndex];
 
-  const nextIndex = selectedIndex + 1 >= activeAgents.length
-    ? 0
-    : selectedIndex + 1;
+//   const nextIndex = selectedIndex + 1 >= activeAgents.length
+//     ? 0
+//     : selectedIndex + 1;
 
-  const nextAgentSequence = Number(activeAgents[nextIndex].sequence);
+//   const nextAgentSequence = Number(activeAgents[nextIndex].sequence);
 
-  distributor.nextAgent = nextAgentSequence;
+//   distributor.nextAgent = nextAgentSequence;
 
-  return {
-    agent: selectedAgent,
-    nextAgentSequence
-  };
-}
+//   return {
+//     agent: selectedAgent,
+//     nextAgentSequence
+//   };
+//}
 
 
 
